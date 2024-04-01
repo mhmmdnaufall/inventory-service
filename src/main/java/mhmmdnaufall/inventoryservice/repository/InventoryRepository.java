6a12852c55c5561d2e267a2b04f42c229f0cbc32
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-04-02 00:48:25 +0700

    Create Inventory Repository

---
+++ src/main/java/mhmmdnaufall/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package mhmmdnaufall.inventoryservice.repository;
+
+import mhmmdnaufall.inventoryservice.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> { }
