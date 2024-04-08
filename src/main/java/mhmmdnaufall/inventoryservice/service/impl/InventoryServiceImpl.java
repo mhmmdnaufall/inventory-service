c3856b34b11f722abd2ab7d4ea25f25818216949
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-04-08 15:08:10 +0700

    Simulating Resilience4j

--- src/main/java/mhmmdnaufall/inventoryservice/service/impl/InventoryServiceImpl.java
+++ src/main/java/mhmmdnaufall/inventoryservice/service/impl/InventoryServiceImpl.java
@@ -1,23 +1,30 @@
 package mhmmdnaufall.inventoryservice.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import mhmmdnaufall.inventoryservice.dto.InventoryResponse;
 import mhmmdnaufall.inventoryservice.repository.InventoryRepository;
 import mhmmdnaufall.inventoryservice.service.InventoryService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor @Slf4j
 public class InventoryServiceImpl implements InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     @Override
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wait Started");
+        Thread.sleep(Duration.ofSeconds(10));
+        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory -> new InventoryResponse(inventory.getSkuCode(), inventory.getQuantity() > 0))
                 .toList();
